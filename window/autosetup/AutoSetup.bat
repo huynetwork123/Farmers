fac986f17f4b42b46b17fc2d84c1df785fc7ecfe
huynetwork123 <phamchanhuy113@gmail.com>
2025-10-17 11:20:32 +0700

    update 17/10/2025

--- window/autosetup/AutoSetup.bat
+++ window/autosetup/AutoSetup.bat
@@ -1,4 +1,4 @@
 @echo off
 :: AutoSetup.bat — run ps1 as admin
 powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-"Start-Process -FilePath 'powershell' -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0AutoSetup.ps1\"' -Verb RunAs"
+"Start-Process -FilePath 'powershell' -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0install-apps.ps1\"' -Verb RunAs"
